e9ad0918138b78ea5a5ecfe926084bf68336e65d
j-isley <79328510+j-isley@users.noreply.github.com>
2021-12-16 13:13:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ data "aws_security_group" "default" {
 }
 
 # Reference Existing Key Pair
-
+/*
 data "aws_key_pair" "aws-TF-1" {
   key_name = "aws-TF-1"
   filter {
@@ -71,7 +71,7 @@ data "aws_key_pair" "aws-TF-1" {
     name = "tag:tool"
     values = ["terraform"]
 }
-
+*/
 # Make NIC
 
 resource "aws_network_interface" "foo" {
